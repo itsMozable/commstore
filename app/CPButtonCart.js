9b71a00082042160b3fb1efe743cf0198bef615a
tattoomozi <tattoomozi@icloud.com>
2023-05-26 16:08:26 +0200

    finetuning

--- app/CPButtonCart.js
+++ app/CPButtonCart.js
@@ -1,12 +1,9 @@
 'use client';
 import './CPButtons.scss';
 
-export default function CPButtonRedGlow() {
+export default function CPButtonPurple() {
   return (
-    <button
-      className="cyberpunk2077 redGlow"
-      data-test-id="product-add-to-cart"
-    >
+    <button className="cyberpunk2077 purple" data-test-id="product-add-to-cart">
       Add to cart
     </button>
   );
--- app/Products/Livestock/page.js
+++ app/Products/Livestock/page.js
@@ -2,11 +2,12 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { lsExhibits } from '../../../database/finalExhibits';
 import CPButtonCart from '../../CPButtonCart';
+import CPButtonPurple from '../../CPButtonCart';
 
 export default function LivestockPage() {
   return (
     <main className="lsMain">
-      <h1 className="cyberpunk">Samurai Livestock</h1>
+      <h1 className="cyberpunk glitched">Samurai Livestock</h1>
       <h2>something to eat - something to fight</h2>
       {lsExhibits.map((exhibits) => {
         return (
@@ -26,7 +27,7 @@ export default function LivestockPage() {
               {exhibits.name} {': '}
               {exhibits.type}{' '}
             </Link>
-            <CPButtonCart />
+            <CPButtonPurple />
           </div>
         );
       })}
